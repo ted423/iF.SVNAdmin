60f1e3e0df67b4278b19617adb732e93a5b8c889
ted423 <priston910423@gmail.com>
2026-08-07 14:48:44 +0800

    fix: no tab

--- include/ifcorelib/IF_HtPasswd.class.php
+++ include/ifcorelib/IF_HtPasswd.class.php
@@ -30,54 +30,54 @@
  */
 class IF_HtPasswd
 {
-	// Holds the user file as an array (username=>encrypted-password)
-	private $m_data = array();
+  // Holds the user file as an array (username=>encrypted-password)
+  private $m_data = array();
 
-	// Holds the path to the user authentication file.
-	private $m_userfile = NULL;
+  // Holds the path to the user authentication file.
+  private $m_userfile = NULL;
 
-	// Holds the error number, if a error occured.
-	private $m_errno = 0;
+  // Holds the error number, if a error occured.
+  private $m_errno = 0;
 
-	//////////////////////////////////////////////////////////////////////////////
+  //////////////////////////////////////////////////////////////////////////////
 
-	/**
-	 * Creates a new instance of this class and assigns the given
-	 * file as "passwd" file to it.
-	 *
-	 * @param string $userfile
-	 */
-	public function __construct( $userfile )
-	{
-		$this->m_userfile = $userfile;
-	}
+  /**
+   * Creates a new instance of this class and assigns the given
+   * file as "passwd" file to it.
+   *
+   * @param string $userfile
+   */
+  public function __construct( $userfile )
+  {
+    $this->m_userfile = $userfile;
+  }
 
-	/**
-	 * Loads the file content and does some init operations.
-	 *
-	 * @return void
-	 */
+  /**
+   * Loads the file content and does some init operations.
+   *
+   * @return void
+   */
   public function init()
   {
-  	$b = self::parseUserFile( $this->m_userfile );
-  	return $b;
+    $b = self::parseUserFile( $this->m_userfile );
+    return $b;
   }
 
   public function errno()
   {
-  	return $this->m_errno;
+    return $this->m_errno;
   }
 
   public function error()
   {
-  	switch( $this->m_errno )
-  	{
-  		case 1: return "The user authentication file does not exist.";
-  		case 2: return "No READ permission on the user authentication file.";
-  		case 10: return "The user already exists.";
-  		case 11: return "The user does not exist.";
-  		default: return "No error occured.";
-  	}
+    switch( $this->m_errno )
+    {
+      case 1: return "The user authentication file does not exist.";
+      case 2: return "No READ permission on the user authentication file.";
+      case 10: return "The user already exists.";
+      case 11: return "The user does not exist.";
+      default: return "No error occured.";
+    }
   }
 
   /**
@@ -87,12 +87,12 @@ class IF_HtPasswd
    */
   public function getUserList()
   {
-  	$retUsers = array();
-  	foreach( $this->m_data as $username=>$pass )
-  	{
-  		array_push( $retUsers, $username );
-  	}
-  	return $retUsers;
+    $retUsers = array();
+    foreach( $this->m_data as $username=>$pass )
+    {
+      array_push( $retUsers, $username );
+    }
+    return $retUsers;
   }
 
   /**
@@ -107,18 +107,18 @@ class IF_HtPasswd
   {
     if( self::userExists( $username ) )
     {
-    	// The user already exists.
+      // The user already exists.
       $this->m_errno = 10;
       return false;
     }
 
-  	// Should the password being crypted?
-  	if( $crypt == true )
-  	{
-  		$password = self::crypt_default( $password ); // Force MD5 as salt!
-  	}
+    // Should the password being crypted?
+    if( $crypt == true )
+    {
+      $password = self::crypt_default( $password ); // Force MD5 as salt!
+    }
 
-  	// Add the user to the holded data array.
+    // Add the user to the holded data array.
     $this->m_data[$username] = $password;
     return true;
   }
@@ -142,13 +142,13 @@ class IF_HtPasswd
   {
     if( !self::userExists( $username ) )
     {
-    	// The user does not exists.
-    	$this->m_errno = 11;
-    	return false;
+      // The user does not exists.
+      $this->m_errno = 11;
+      return false;
     }
     else
     {
-    	// Unset user.
+      // Unset user.
       unset( $this->m_data[$username] );
       return true;
     }
@@ -156,96 +156,96 @@ class IF_HtPasswd
 
   public function userExists( $username )
   {
-  	if( isset( $this->m_data[$username] ) && !empty( $this->m_data[$username] ) )
-  	{
-  		return true;
-  	}
-  	else
-  	{
-  		return false;
-  	}
+    if( isset( $this->m_data[$username] ) && !empty( $this->m_data[$username] ) )
+    {
+      return true;
+    }
+    else
+    {
+      return false;
+    }
   }
 
   public function authenticate( $username, $password )
   {
-  	// Find the user.
+    // Find the user.
     foreach( $this->m_data as $usr=>&$pass )
     {
-    	// Found user.
+      // Found user.
       if( $usr == $username )
       {
-      	// Find out which encryption type is used.
-      	// SHA
-      	if (strpos($pass, "{SHA}") === 0)
-      	{
-      		$password_crypted = self::crypt_sha($password);
+        // Find out which encryption type is used.
+        // SHA
+        if (strpos($pass, "{SHA}") === 0)
+        {
+          $password_crypted = self::crypt_sha($password);
 
-//      		echo "Type: SHA\n";
+//          echo "Type: SHA\n";
 //          echo "Password-In-File: ".$pass."\n";
-//      		echo "Password-Crypted: ".$password_crypted."\n";
+//          echo "Password-Crypted: ".$password_crypted."\n";
 
-      		if ($password_crypted == $pass)
-      		  return true;
-      		else
-      		  return false;
-      	}
-		// BCrypt/Blowfish
-		elseif (strpos($pass, '$2') === 0 && preg_match('/^\$2([abxy])\$(\d+)\$(.{22}).{31}$/', $pass, $matches) > 0)
-		{
-			$blowfish_version = $matches[1];
-			$cost = (int)$matches[2];
-			$salt = $matches[3];
-			$password_crypted = self::crypt_blowfish($password, $cost, $salt, $blowfish_version);
+          if ($password_crypted == $pass)
+            return true;
+          else
+            return false;
+        }
+    // BCrypt/Blowfish
+    elseif (strpos($pass, '$2') === 0 && preg_match('/^\$2([abxy])\$(\d+)\$(.{22}).{31}$/', $pass, $matches) > 0)
+    {
+      $blowfish_version = $matches[1];
+      $cost = (int)$matches[2];
+      $salt = $matches[3];
+      $password_crypted = self::crypt_blowfish($password, $cost, $salt, $blowfish_version);
 
-			if ($password_crypted == $pass)
-				return true;
-			else
-				continue;
-		}
-      	// MD5
-      	elseif (strpos($pass, '$apr1$') === 0 && preg_match('/\$(.*)\$(.*)\$(.*)/', $pass, $matches) > 0)
-      	{
-      		// Stick togehter the salt of the password.
-      		$salt = $matches[2];
+      if ($password_crypted == $pass)
+        return true;
+      else
+        continue;
+    }
+        // MD5
+        elseif (strpos($pass, '$apr1$') === 0 && preg_match('/\$(.*)\$(.*)\$(.*)/', $pass, $matches) > 0)
+        {
+          // Stick togehter the salt of the password.
+          $salt = $matches[2];
 
-      		// Crypt the user entered password.
-      		$password_crypted = self::crypt_apr1_md5($password, $salt);
+          // Crypt the user entered password.
+          $password_crypted = self::crypt_apr1_md5($password, $salt);
 
 //          echo "Type: MD5\n";
 //          echo "Password-In-File: ".$pass."\n";
 //          echo "Password-Salt   : ".$salt."\n";
 //          echo "Password-Crypted: ".$password_crypted."\n";
 
-      		if ($password_crypted == $pass)
-      		  return true;
-      		else
-      		  return false;
-      	}
-      	// CRYPT (only unix)
-      	else
-      	{
-      		// The different length of salts.
-      		$crypt_types = array("STD-DES" => 2, "EXT-DES" => 9, "MD5" => 12, "BLOWFISH" => 16);
+          if ($password_crypted == $pass)
+            return true;
+          else
+            return false;
+        }
+        // CRYPT (only unix)
+        else
+        {
+          // The different length of salts.
+          $crypt_types = array("STD-DES" => 2, "EXT-DES" => 9, "MD5" => 12, "BLOWFISH" => 16);
 
-      		foreach ($crypt_types as $type=>$len)
-      		{
-      			$salt = substr($pass, 0, $len);
-      			$password_crypted = self::crypt_unix($password, $salt);
+          foreach ($crypt_types as $type=>$len)
+          {
+            $salt = substr($pass, 0, $len);
+            $password_crypted = self::crypt_unix($password, $salt);
 
-//	          echo "Type: CRYPT (Unix only)\n";
-//	          echo "Hash-Type: ".$type."\n";
-//	          echo "Password-In-File: ".$pass."\n";
-//	          echo "Password-Salt   : ".$salt."\n";
-//	          echo "Password-Crypted: ".$password_crypted."\n";
-//	          echo "\n";
+//            echo "Type: CRYPT (Unix only)\n";
+//            echo "Hash-Type: ".$type."\n";
+//            echo "Password-In-File: ".$pass."\n";
+//            echo "Password-Salt   : ".$salt."\n";
+//            echo "Password-Crypted: ".$password_crypted."\n";
+//            echo "\n";
 
-	          if ($password_crypted == $pass)
-	            return true;
-	          else
-	            continue;
-      		}
-      		return false;
-      	}
+            if ($password_crypted == $pass)
+              return true;
+            else
+              continue;
+          }
+          return false;
+        }
       }
     }
 
@@ -266,16 +266,16 @@ class IF_HtPasswd
   {
     if( !file_exists( $userfile ) )
     {
-    	// File does not exist.
-    	$this->m_errno = 1;
-    	return false;
+      // File does not exist.
+      $this->m_errno = 1;
+      return false;
     }
 
     if( !is_readable( $userfile  ) )
     {
-    	// No permission to read the file.
-    	$this->m_errno = 2;
-    	return false;
+      // No permission to read the file.
+      $this->m_errno = 2;
+      return false;
     }
 
     // Open file in read-mode.
@@ -290,7 +290,7 @@ class IF_HtPasswd
 
       if( empty( $line ) )
       {
-      	continue;
+        continue;
       }
 
       // Split the line by ':'.
@@ -343,75 +343,75 @@ class IF_HtPasswd
    */
   private function crypt_default($plainpasswd)
   {
-  	$type = "";
-  	if (defined("IF_HtPasswd_DefaultCrypt"))
-  	{
-  		$type = IF_HtPasswd_DefaultCrypt;
-  	}
+    $type = "";
+    if (defined("IF_HtPasswd_DefaultCrypt"))
+    {
+      $type = IF_HtPasswd_DefaultCrypt;
+    }
 
-  	switch ($type)
-  	{
-		case "CRYPT":
-			return self::crypt_unix($plainpasswd);
+    switch ($type)
+    {
+    case "CRYPT":
+      return self::crypt_unix($plainpasswd);
 
-		case "BLOWFISH":
-			return self::crypt_blowfish($plainpasswd);
+    case "BLOWFISH":
+      return self::crypt_blowfish($plainpasswd);
 
-  		case "SHA1":
-  			return self::crypt_sha($plainpasswd);
+      case "SHA1":
+        return self::crypt_sha($plainpasswd);
 
-  		case "MD5":
-  			return self::crypt_apr1_md5($plainpasswd);
+      case "MD5":
+        return self::crypt_apr1_md5($plainpasswd);
 
-  		default:
-  			return self::crypt_apr1_md5($plainpasswd);
-  	}
+      default:
+        return self::crypt_apr1_md5($plainpasswd);
+    }
   }
 
-	/**
-	 * Creates a default unix crypt hash of the given password with the
-	 * specified salt. If no salt is given then the function will use its own
-	 * generated salt.
-	 *
-	 * @param string $plainpasswd
-	 * @param string $salt
-	 * @return string
-	 */
-	private function crypt_unix($plainpasswd, $salt = "")
-	{
-		// If no salt is defined, let's use blowfish (with a salt)
-		if (empty($salt)) {
-			return self::crypt_blowfish($plainpasswd);
-		}
-		return crypt($plainpasswd, $salt);
-	}
+  /**
+   * Creates a default unix crypt hash of the given password with the
+   * specified salt. If no salt is given then the function will use its own
+   * generated salt.
+   *
+   * @param string $plainpasswd
+   * @param string $salt
+   * @return string
+   */
+  private function crypt_unix($plainpasswd, $salt = "")
+  {
+    // If no salt is defined, let's use blowfish (with a salt)
+    if (empty($salt)) {
+      return self::crypt_blowfish($plainpasswd);
+    }
+    return crypt($plainpasswd, $salt);
+  }
 
-	/**
-	 * Creates a default blowfish hash of the given password with the
-	 * specified salt. If no salt is given then the function will use its own
-	 * generated salt.
-	 *
-	 * @param string $plainpasswd
-	 * @param int $cost
-	 * @param string $salt
-	 * @param string $blowfish_version
-	 * @return string
-	 */
-	private function crypt_blowfish($plainpasswd, $cost = 12, $salt = "", $blowfish_version = 'y')
-	{
-		// Provide a salt anyway
-		if (empty($salt)) {
-			$salt = substr(base64_encode(openssl_random_pseudo_bytes(17)), 0, 22);
-			$salt = str_replace("+", ".", $salt);
-		}
-		$blowfish = sprintf(
-			'$2%s$%02s$%s',
-			$blowfish_version,
-			$cost,
-			$salt
-		);
-		return crypt($plainpasswd, $blowfish);
-	}
+  /**
+   * Creates a default blowfish hash of the given password with the
+   * specified salt. If no salt is given then the function will use its own
+   * generated salt.
+   *
+   * @param string $plainpasswd
+   * @param int $cost
+   * @param string $salt
+   * @param string $blowfish_version
+   * @return string
+   */
+  private function crypt_blowfish($plainpasswd, $cost = 12, $salt = "", $blowfish_version = 'y')
+  {
+    // Provide a salt anyway
+    if (empty($salt)) {
+      $salt = substr(base64_encode(openssl_random_pseudo_bytes(17)), 0, 22);
+      $salt = str_replace("+", ".", $salt);
+    }
+    $blowfish = sprintf(
+      '$2%s$%02s$%s',
+      $blowfish_version,
+      $cost,
+      $salt
+    );
+    return crypt($plainpasswd, $blowfish);
+  }
 
   /**
    * Creates an SHA1 generated hash of the given plain text password.
@@ -421,8 +421,8 @@ class IF_HtPasswd
    */
   private function crypt_sha($plainpasswd)
   {
-  	$password_crypted = "{SHA}".base64_encode(sha1($plainpasswd, true));
-  	return $password_crypted;
+    $password_crypted = "{SHA}".base64_encode(sha1($plainpasswd, true));
+    return $password_crypted;
   }
 
   /**
@@ -434,58 +434,58 @@ class IF_HtPasswd
    * @param string $salt
    * @return string Hash of the plain password.
    */
-	private function crypt_apr1_md5($plainpasswd, $salt = "")
-	{
-		// Use default salt?
-		$translate_to = "./0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-		if (empty($salt))
-		{
-			$salt = substr(str_shuffle($translate_to), 0, 8);
-		}
+  private function crypt_apr1_md5($plainpasswd, $salt = "")
+  {
+    // Use default salt?
+    $translate_to = "./0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    if (empty($salt))
+    {
+      $salt = substr(str_shuffle($translate_to), 0, 8);
+    }
 
-		// Password length.
-		$len = strlen($plainpasswd);
-		$text = $plainpasswd.'$apr1$'.$salt;
-		$bin = pack("H32", md5($plainpasswd.$salt.$plainpasswd));
+    // Password length.
+    $len = strlen($plainpasswd);
+    $text = $plainpasswd.'$apr1$'.$salt;
+    $bin = pack("H32", md5($plainpasswd.$salt.$plainpasswd));
 
-		for($i=$len; $i>0; $i-=16)
-		{
-			$text.= substr($bin, 0, min(16, $i));
-		}
+    for($i=$len; $i>0; $i-=16)
+    {
+      $text.= substr($bin, 0, min(16, $i));
+    }
 
-		for($i=$len; $i>0; $i>>=1)
-		{
-			$text.= ($i & 1) ? chr(0) : $plainpasswd[0];
-		}
+    for($i=$len; $i>0; $i>>=1)
+    {
+      $text.= ($i & 1) ? chr(0) : $plainpasswd[0];
+    }
 
-		$bin = pack("H32", md5($text));
+    $bin = pack("H32", md5($text));
 
-		for($i = 0; $i < 1000; $i++)
-		{
-			$new = ($i & 1) ? $plainpasswd : $bin;
-			if ($i % 3) $new .= $salt;
-			if ($i % 7) $new .= $plainpasswd;
-			$new .= ($i & 1) ? $bin : $plainpasswd;
-			$bin = pack("H32", md5($new));
-		}
+    for($i = 0; $i < 1000; $i++)
+    {
+      $new = ($i & 1) ? $plainpasswd : $bin;
+      if ($i % 3) $new .= $salt;
+      if ($i % 7) $new .= $plainpasswd;
+      $new .= ($i & 1) ? $bin : $plainpasswd;
+      $bin = pack("H32", md5($new));
+    }
 
-		$tmp = "";
-		for ($i = 0; $i < 5; $i++)
-		{
-			$k = $i + 6;
-			$j = $i + 12;
-			if ($j == 16) $j = 5;
-			$tmp = $bin[$i].$bin[$k].$bin[$j].$tmp;
-		}
+    $tmp = "";
+    for ($i = 0; $i < 5; $i++)
+    {
+      $k = $i + 6;
+      $j = $i + 12;
+      if ($j == 16) $j = 5;
+      $tmp = $bin[$i].$bin[$k].$bin[$j].$tmp;
+    }
 
-		$tmp = chr(0).chr(0).$bin[11].$tmp;
-		$tmp = strtr(
-			  strrev(substr(base64_encode($tmp), 2)),
-			  "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/",
-			  $translate_to
-		  );
+    $tmp = chr(0).chr(0).$bin[11].$tmp;
+    $tmp = strtr(
+        strrev(substr(base64_encode($tmp), 2)),
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/",
+        $translate_to
+      );
 
-		return "$"."apr1"."$".$salt."$".$tmp;
-	}
+    return "$"."apr1"."$".$salt."$".$tmp;
+  }
 }
 ?>
